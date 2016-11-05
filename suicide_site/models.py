922f0ef2e76ca22fec9f213fc3d9bff16b0f67ad
Greg McCoy <gregmccoy@gfa.org>
2016-11-05 15:52:46 -0400

    Adding title to Reason model

--- suicide_site/models.py
+++ suicide_site/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 # Create your models here.
 class Reason(models.Model):
+    title = models.CharField(max_length=200, default='')
     keywords = models.TextField()
     resources = models.TextField()
     response = models.TextField()
+
+    def __str__(self):
+        return u"%s" % (self.title)
