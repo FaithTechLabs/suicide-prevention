8db429bd323ec369959271cd0a8d64b10a0db9cb
Greg McCoy <gregmccoy@gfa.org>
2016-11-05 17:23:32 -0400

    Fixing IP

--- suicide_site/views.py
+++ suicide_site/views.py
@@ -5,10 +5,8 @@ from .models import Reason
 
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
+    ip = request.META.get('HTTP_CF_CONNECTING_IP')
+    if ip is None:
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
