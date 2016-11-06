74e2c32447868d5910a70566ff82c706fbbe3a4e
sgault <sarah@bidvine.com>
2016-11-06 00:12:39 -0400

    Added nav menu for mobile, fixed css bugs, added crisis centre names

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -1,49 +1,84 @@
 $(document).foundation()
 
+var navClick = function() {
+  $('.nav ul').toggleClass('hide-for-small-only');
+}
+
+var formatPhone = function(num) {
+  num = num.toString();
+  var phone = num[0];
+  phone += ' (' + num.slice(1, 4) + ') ';
+  phone += num.slice(4, 7) + '-' + num.slice(7);
+  return phone;
+}
+
 var initLocation = function(loc) {
-  var phone;
+  var province;
   switch(loc) {
     case 'AB':
-      phone = '+14032264357';
-      break;
     case 'BC':
-      phone = '+18007842433';
-      break;
     case 'MB':
-      phone = '+18774357170';
-      break;
     case 'NB':
-      phone = '+18006675005';
-      break;
     case 'NL':
-      phone = '+18887374668';
-      break;
     case 'NS':
-      phone = '+18884298167';
-      break;
     case 'NT':
-      // 7-11pm (?) resource
-      phone = '+18006610844';
-      break;
+    case 'ON':
     case 'PE':
-      phone = '+18002182885';
-      break;
     case 'QC':
-      phone = '+18662773553';
-      break;
     case 'SK':
-      phone = '+13067570127';
-      break;
     case 'NU':
+      obj = loc;
+      break;
     case 'YT':
       // 24/7 service for Nunavut
-      phone = '+18002653333';
+      obj = 'NU';
       break;
     default:
       // Ontario number
-      phone = '+18665312600';
-      break;
+      obj = 'ON';
   }
 
-  $('#phoneButton').attr('href', 'tel:' + phone);
+  var province = centres[obj];
+  $('.phone-btn').attr('href', 'tel:+' + province.phone);
+  $('.phone-btn .number').text(formatPhone(province.phone));
+  $('.phone-btn .name').html(province.name)
+}
+
+var centres = {
+  AB: {
+    name: 'Distress Centre Calgary',
+    phone: 14032264357
+  },
+  BC: {
+    name: 'Crisis Centre of BC',
+    phone: 18007842433
+  },
+  MB: {
+    name: 'Reason To Live Manitoba',
+    phone: 18774357170
+  },
+  NB: {
+    name: 'Chimo Helpline New Brunswick',
+    phone: 18006675005
+  },
+  NT: {
+    name: 'NWT Help Line',
+    phone: 18006610844
+  },
+  NU: {
+    name: 'Kamatsiaqtut Help Line',
+    phone: 18002653333
+  },
+  ON: {
+    name: 'Mental Health Helpline Ontario',
+    phone: 18665312600
+  },
+  QC: {
+    name: 'Association québécoise de prévention du suicide',
+    phone: 18662773553
+  },
+  SK: {
+    name: 'Mobile Crisis Services Saskatchewan',
+    phone: 13067570127
+  }
 }
